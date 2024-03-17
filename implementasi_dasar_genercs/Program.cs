0331a30196c54aa1fed39fcdc42ecc791214c9c3
Syahreza Adnan Al Azhar <notsupersyahreza@student.telkomuniversity.ac.id>
2024-03-18 00:47:42 +0700

    menambahkan class DataGeneric

--- implementasi_dasar_genercs/Program.cs
+++ implementasi_dasar_genercs/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Reflection;
+
+public class HaloGeneric
+{
+    public void SapaUser<T>(T inputUser)
+    {
+        Console.WriteLine($"Halo user {inputUser}");
+    }
+}
+
+public class DataGeneric<T>
+{
+    private T data;
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine($"Data yang tersimpan adalah: {this.data}");
+    }
+}
+
+public class main
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Masukkan nama anda: ");
+        String inputUser = Console.ReadLine();
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser(inputUser);
+
+        DataGeneric<int> dataNIM = new DataGeneric<int>(1302223041);
+        dataNIM.PrintData();
+    }
+}
